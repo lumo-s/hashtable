89b6384451e2899570d7406aa7aff5169601cb98
lumo-s <111223895+lumo-s@users.noreply.github.com>
2022-10-09 22:04:18 +0800

    Add files via upload

---
+++ Leetcode387.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.leetcode387;
+
+/**
+ *
+ * @author 86139
+ */
+public class Leetcode387 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
